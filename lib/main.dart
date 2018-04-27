7b66dccc43affb16f47a8e6cd51208610ef91ea9
Esron Silva <esron.dtamar@gmail.com>
2018-04-27 08:34:09 -0300

    added box in the middle

--- lib/main.dart
+++ lib/main.dart
@@ -36,73 +36,76 @@ class LoginPageState extends State<LoginPage>
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      backgroundColor: Colors.greenAccent,
       body: new Stack(
         fit: StackFit.expand,
         children: <Widget>[
           new Image(
             image: new AssetImage('assets/background.jpg'),
-            fit: BoxFit.cover,
+            fit: BoxFit.fill,
           ),
-          new Container(
-            padding: EdgeInsets.all(40.0),
-            color: Colors.white30,
-            child: new Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                new Image(
-                  image: new AssetImage('assets/logo.png'),
-                  width: _iconAnimation.value * 100,
-                ),
-                new Form(
-                  child: Theme(
-                    data: new ThemeData(
-                        brightness: Brightness.dark,
-                        primarySwatch: Colors.teal,
-                        inputDecorationTheme: new InputDecorationTheme(
-                            labelStyle: new TextStyle(
-                                color: Colors.white, fontSize: 20.0))),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        new TextFormField(
-                          decoration: new InputDecoration(
-                            labelText: 'Enter Email',
-                          ),
-                          keyboardType: TextInputType.emailAddress,
-                        ),
-                        new TextFormField(
-                          decoration: new InputDecoration(
-                            labelText: 'Enter Password',
-                          ),
-                          keyboardType: TextInputType.text,
-                          obscureText: true,
-                        ),
-                        new Padding(
-                          padding: const EdgeInsets.only(top: 20.0),
-                        ),
-                        new MaterialButton(
-                          minWidth: 150.0,
-                          color: Colors.redAccent,
-                          textColor: Colors.black,
-                          child: new Text('Sign in'),
-                          onPressed: () => {},
-                        ),
-                        new Padding(
-                          padding: const EdgeInsets.only(top: 8.0),
-                        ),
-                        new MaterialButton(
-                          minWidth: 150.0,
-                          color: Colors.white70,
-                          textColor: Colors.black87,
-                          child: new Text('Sign up'),
-                          onPressed: () => {},
-                        )
-                      ],
-                    ),
+          new Center(
+            child: new Container(
+              width: 300.0, 
+              height: 350.0,
+              padding: EdgeInsets.all(10.0),
+              color: Colors.white30,
+              child: new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new Image(
+                    image: new AssetImage('assets/logo.png'),
+                    width: _iconAnimation.value * 100,
                   ),
-                )
-              ],
+                  new Form(
+                    child: Theme(
+                      data: new ThemeData(
+                          brightness: Brightness.dark,
+                          primarySwatch: Colors.teal,
+                          inputDecorationTheme: new InputDecorationTheme(
+                              labelStyle: new TextStyle(
+                                  color: Colors.white, fontSize: 20.0))),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          new TextFormField(
+                            decoration: new InputDecoration(
+                              labelText: 'Enter Email',
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                          new TextFormField(
+                            decoration: new InputDecoration(
+                              labelText: 'Enter Password',
+                            ),
+                            keyboardType: TextInputType.text,
+                            obscureText: true,
+                          ),
+                          new Padding(
+                            padding: const EdgeInsets.only(top: 20.0),
+                          ),
+                          new MaterialButton(
+                            minWidth: 150.0,
+                            color: Colors.redAccent,
+                            textColor: Colors.black,
+                            child: new Text('Sign in'),
+                            onPressed: () => {},
+                          ),
+                          new Padding(
+                            padding: const EdgeInsets.only(top: 8.0),
+                          ),
+                          new MaterialButton(
+                            minWidth: 150.0,
+                            color: Colors.white70,
+                            textColor: Colors.black87,
+                            child: new Text('Sign up'),
+                            onPressed: () => {},
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           )
         ],
